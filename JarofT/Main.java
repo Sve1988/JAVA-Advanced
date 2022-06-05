7e285fa99172ca583994dbaf62d824a82225f092
Sve1988 <68242735+Sve1988@users.noreply.github.com>
2022-06-05 11:22:16 +0300

    Add files via upload

---
+++ JarofT/Main.java
@@ -0,0 +1,15 @@
+package JarofT;
+
+import Classes.JarofT.Jar;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<String> jar =new Jar<>();
+
+        jar.add("First");
+        jar.add("Second");
+        jar.add("Third");
+
+        System.out.println(jar.remove());
+    }
+}
